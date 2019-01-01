dcc4d4a86cecd62d42f697221dc5e65468ac2701
ixpert <amr.elnagar@outlook.com>
2019-01-01 02:31:35 +0100

    Initial commit

---
+++ src/main/java/com/ixpert/thymeleafandsecurity/ThymeleafandsecurityApplication.java
@@ -0,0 +1,14 @@
+package com.ixpert.thymeleafandsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafandsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafandsecurityApplication.class, args);
+    }
+
+}
+
